667e1e0da0af8550f2847527140e74bb713abab2
Shashwat <awesomedude019@gmail.com>
2025-10-13 01:58:21 +0530

    forgot to add trpc

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -2,8 +2,13 @@ import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from "@neondatabase/serverless";
 
-
-const sql = neon(process.env.DATABASE_URL!);
+const sql = (() => {
+	const url = process.env.DATABASE_URL;
+	if (!url) {
+		throw new Error('DATABASE_URL environment variable is not set.');
+	}
+	return neon(url);
+})();
 
 export const db = drizzle(sql);
 
---
+++ src/server/trpc/trpc.ts
@@ -0,0 +1,7 @@
+/*ignore es-lint */
+import { initTRPC } from "@trpc/server";
+
+const t= initTRPC.create();
+
+export const router = t.router;
+export const publicProcedure = t.procedure;
